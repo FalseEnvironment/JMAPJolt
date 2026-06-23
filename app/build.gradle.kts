d7879069f07659ddf262ef3d62687e3d8d89ffce
FalseEnvironment <mail@lukeking12.net>
2026-06-23 20:06:10 +0200

    Bump versionCode 8 / versionName 0.17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.falseenvironment.jmapjolt"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "0.16"
+        versionCode = 8
+        versionName = "0.17"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
